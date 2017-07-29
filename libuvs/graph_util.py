cf7d80d14f5a00f6a9faf4ae9ba5cec334171f26
kouritron <kourosh.lk.kh@gmail.com>
2017-07-29 13:19:30 -0400

    fast algo to find eca, tests and benchmarks for find eca

--- libuvs/graph_util.py
+++ libuvs/graph_util.py
@@ -15,6 +15,10 @@ class Queue():
         return 0 == len(self.queue_items)
 
 
+    def has_items(self):
+        return 0 != len(self.queue_items)
+
+
     def size(self):
 
         return len(self.queue_items)
@@ -30,9 +34,40 @@ class Queue():
 
 
 # a graph class that export a unified interface to get the neighbors of some node, regardless of whether the DAG
-# is in memory or on disk, or some combination of that.
+# is in memory or on disk, or some combination of that + some utilities like counting DAG lookups (for benchmarking)
 class DAG():
-    pass
+
+    def __init__(self, graph_adjacencies):
+
+        # adjacencies must be a dict of node to a list of its parents
+        assert isinstance(graph_adjacencies, dict)
+
+        self.adjacencies = graph_adjacencies
+
+        self.lookup_count = 0
+
+
+    def reset_lookup_count(self):
+
+        self.lookup_count = 0
+
+
+    def get_lookup_count(self):
+
+        return self.lookup_count
+
+
+    def get_neighbors(self, node):
+
+        self.lookup_count = self.lookup_count + 1
+
+        if node in self.adjacencies:
+            return self.adjacencies[node]
+
+        else:
+            assert False, "Invalid DAG lookup"
+
+
 
 
 
@@ -42,13 +77,12 @@ class DAG():
 # the slow algo has  O(2n) space and time complexity
 # considering the graph is sitting on disk, 2n look ups may very well be unacceptable for a repo with
 # few hundred kilo commits in it.
-def dag_find_eca_slow(graph_adjacencies, node1, node2):
+def dag_find_eca_slow(dag, node1, node2):
     """ Find the earliest common ancestor of the nodes in a regular DAG (DAG with parent pointers
     as opposed to kid pointers)
     """
 
-    # adjacencies must be a dict of node to a list of its parents
-    assert isinstance(graph_adjacencies, dict)
+    assert isinstance(dag, DAG)
 
     # parent and node_to_test muse be snapids as str or unicode.
     assert isinstance(node1, str) or isinstance(node1, unicode)
@@ -60,14 +94,13 @@ def dag_find_eca_slow(graph_adjacencies, node1, node2):
     node1_q = Queue()
 
     node1_q.enqueue(node1)
+    node1_ancestors.add(node1)
 
-    while not node1_q.is_empty():
+    while node1_q.has_items():
 
         node1_ancestor = node1_q.dequeue()
 
-        node1_ancestors.add(node1_ancestor)
-
-        for node1_ancestor_parent in graph_adjacencies[node1_ancestor]:
+        for node1_ancestor_parent in dag.get_neighbors(node1_ancestor):
 
             # if this is a new ancestor
             if node1_ancestor_parent not in node1_ancestors:
@@ -79,16 +112,17 @@ def dag_find_eca_slow(graph_adjacencies, node1, node2):
     node2_q = Queue()
     node2_bfs_seen_before = set()
 
+    node2_bfs_seen_before.add(node2)
     node2_q.enqueue(node2)
 
-    while not node2_q.is_empty():
+    while node2_q.has_items():
 
         node2_ancestor = node2_q.dequeue()
 
         if node2_ancestor in node1_ancestors:
             return node2_ancestor
 
-        for node2_ancestor_parent in graph_adjacencies[node2_ancestor]:
+        for node2_ancestor_parent in dag.get_neighbors(node2_ancestor):
 
             if node2_ancestor_parent not in node2_bfs_seen_before:
                 node2_q.enqueue(node2_ancestor_parent)
@@ -101,15 +135,80 @@ def dag_find_eca_slow(graph_adjacencies, node1, node2):
     assert False, "invalid DAG"
 
 
+# this algo is faster but i am not sure its 100% correct
+def dag_find_eca_fast(dag, node1, node2):
+    """ Find the earliest common ancestor of the nodes in a regular DAG (DAG with parent pointers
+    as opposed to kid pointers).
+    """
+
+    assert isinstance(dag, DAG)
+
+    # parent and node_to_test muse be snapids as str or unicode.
+    assert isinstance(node1, str) or isinstance(node1, unicode)
+    assert isinstance(node2, str) or isinstance(node2, unicode)
+
+    # make a set of all ancestors
+    n1_ancestors = set()
+    n2_ancestors = set()
+
+    q1 = Queue()
+    q1.enqueue(node1)
+    n1_ancestors.add(node1)
+
+    q2 = Queue()
+    q2.enqueue(node2)
+    n2_ancestors.add(node2)
+
+    while q1.has_items() or q2.has_items():
+
+        #
+        if q1.has_items():
+
+            # get the next ancestor of node 1
+            n1_a = q1.dequeue()
+
+            # if it also is an ancestor of n2, its a common ancestor and
+            if n1_a in n2_ancestors:
+                return n1_a
+
+            for n1_a_parent in dag.get_neighbors(n1_a):
+                if n1_a_parent not in n1_ancestors:
+                    q1.enqueue(n1_a_parent)
+                    n1_ancestors.add(n1_a_parent)
 
 
-def inv_dag_is_descendant_of(graph_adjacencies, parent, node_to_test):
+        #
+        if q2.has_items():
+
+            # get the next ancestor of node 2
+            n2_a = q2.dequeue()
+
+            # if it also is an ancestor of n2, its a common ancestor and
+            if n2_a in n1_ancestors:
+                return n2_a
+
+            for n2_a_parent in dag.get_neighbors(n2_a):
+                if n2_a_parent not in n2_ancestors:
+                    q2.enqueue(n2_a_parent)
+                    n2_ancestors.add(n2_a_parent)
+
+
+
+    # if we have not returned by now,that means that node1 and node2 had no common ancestor.
+    # it also means that the DAG does not have a root that is the ancestors of all nodes, and has disconnected
+    # partitions, this is an error in case of a uvs repo history DAG
+    assert False, "invalid DAG"
+
+
+
+
+
+def inv_dag_is_descendant_of(dag, parent, node_to_test):
     """ Given an inverted DAG (graph with parent to kid pointers), return True if node_to_test is
     a descendant of parent. False otherwise.
     """
 
-    # adjacencies must be a dict of node to a list of its neighbors (kids in this case)
-    assert isinstance(graph_adjacencies, dict)
+    assert isinstance(dag, DAG)
 
     # parent and node_to_test muse be snapids as str or unicode.
     assert isinstance(parent, str) or isinstance(parent, unicode)
@@ -129,14 +228,14 @@ def inv_dag_is_descendant_of(graph_adjacencies, parent, node_to_test):
     bfs_queue.enqueue(parent)
     prev_seen.add(parent)
 
-    while not bfs_queue.is_empty():
+    while bfs_queue.has_items():
 
         current_node = bfs_queue.dequeue()
 
         if current_node == node_to_test:
             return True
 
-        for kid in graph_adjacencies[current_node]:
+        for kid in dag.get_neighbors(current_node):
 
             if kid not in prev_seen:
                 prev_seen.add(kid)
@@ -145,3 +244,22 @@ def inv_dag_is_descendant_of(graph_adjacencies, parent, node_to_test):
 
 
 
+if '__main__' == __name__:
+
+
+    dag1 = {}
+
+    # dag has parent pointers, dvcs nodes have at most 2 parents.
+    dag1['n1'] = []
+    dag1['n2'] = ['n1']
+    dag1['n3'] = ['n1']
+    dag1['n4'] = ['n2', 'n3']
+    dag1['n5'] = ['n3']
+    dag1['n6'] = ['n4', 'n5']
+    dag1['n7'] = ['n2']
+    dag1['n8'] = ['n7']
+    dag1['n9'] = ['n7']
+    dag1['n10'] = ['n8', 'n9']
+
+
+    print dag_find_eca_fast(dag1, node1='n2', node2='n3')
--- tests/test_graph_util.py
+++ tests/test_graph_util.py
@@ -25,39 +25,40 @@ class TestGraphUtil(unittest.TestCase):
         super(TestGraphUtil, cls).setUpClass()
 
         # set up some sample DAGs
-        cls.invdag1 = {}
+        invdag1_adjacencies = {}
 
-        cls.invdag1['n1'] = ['n2', 'n3']
-        cls.invdag1['n2'] = ['n22', 'n7']
-        cls.invdag1['n3'] = ['n4']
-        cls.invdag1['n4'] = ['n7', 'n10', 'n92']
-        cls.invdag1['n22'] = []
-        cls.invdag1['n7'] = ['n71']
-        cls.invdag1['n10'] = ['n71']
-        cls.invdag1['n92'] = ['n81', 'n82', 'n85', 'n87']
-        cls.invdag1['n71'] = []
-        cls.invdag1['n81'] = []
-        cls.invdag1['n82'] = []
-        cls.invdag1['n85'] = []
-        cls.invdag1['n87'] = []
+        invdag1_adjacencies['n1'] = ['n2', 'n3']
+        invdag1_adjacencies['n2'] = ['n22', 'n7']
+        invdag1_adjacencies['n3'] = ['n4']
+        invdag1_adjacencies['n4'] = ['n7', 'n10', 'n92']
+        invdag1_adjacencies['n22'] = []
+        invdag1_adjacencies['n7'] = ['n71']
+        invdag1_adjacencies['n10'] = ['n71']
+        invdag1_adjacencies['n92'] = ['n81', 'n82', 'n85', 'n87']
+        invdag1_adjacencies['n71'] = []
+        invdag1_adjacencies['n81'] = []
+        invdag1_adjacencies['n82'] = []
+        invdag1_adjacencies['n85'] = []
+        invdag1_adjacencies['n87'] = []
 
+        cls.invdag1 = gu.DAG(invdag1_adjacencies)
 
         # set up some sample DAGs
-        cls.dag1 = {}
+        dag1_adjacencies = {}
 
         # dag has parent pointers, dvcs nodes have at most 2 parents.
-        cls.dag1['n1'] = []
-        cls.dag1['n2'] = ['n1']
-        cls.dag1['n3'] = ['n1']
-        cls.dag1['n4'] = ['n2', 'n3']
-        cls.dag1['n5'] = ['n3']
-        cls.dag1['n6'] = ['n4', 'n5']
-        cls.dag1['n7'] = ['n2']
-        cls.dag1['n8'] = ['n7']
-        cls.dag1['n9'] = ['n7']
-        cls.dag1['n10'] = ['n8', 'n9']
+        dag1_adjacencies['n1'] = []
+        dag1_adjacencies['n2'] = ['n1']
+        dag1_adjacencies['n3'] = ['n1']
+        dag1_adjacencies['n4'] = ['n2', 'n3']
+        dag1_adjacencies['n5'] = ['n3']
+        dag1_adjacencies['n6'] = ['n4', 'n5']
+        dag1_adjacencies['n7'] = ['n2']
+        dag1_adjacencies['n8'] = ['n7']
+        dag1_adjacencies['n9'] = ['n7']
+        dag1_adjacencies['n10'] = ['n8', 'n9']
 
-
+        cls.dag1 = gu.DAG(dag1_adjacencies)
 
 
 
@@ -80,56 +81,101 @@ class TestGraphUtil(unittest.TestCase):
         pass
 
     def test_dag_eca(self):
-        
-        self.assertEquals('n1', gu.dag_find_eca_slow(graph_adjacencies=self.dag1, node1='n2', node2='n3' ))
-        self.assertEquals('n1', gu.dag_find_eca_slow(graph_adjacencies=self.dag1, node1='n1', node2='n2' ))
-        self.assertEquals('n1', gu.dag_find_eca_slow(graph_adjacencies=self.dag1, node1='n2', node2='n1' ))
-        self.assertEquals('n1', gu.dag_find_eca_slow(graph_adjacencies=self.dag1, node1='n1', node2='n5' ))
-        self.assertEquals('n1', gu.dag_find_eca_slow(graph_adjacencies=self.dag1, node1='n2', node2='n5' ))
 
-        self.assertEquals('n2', gu.dag_find_eca_slow(graph_adjacencies=self.dag1, node1='n10', node2='n6' ))
-        self.assertEquals('n1', gu.dag_find_eca_slow(graph_adjacencies=self.dag1, node1='n10', node2='n5' ))
+        test_cases = []
 
+        test_cases.append(['n1', 'n2', 'n3'])
+
+        test_cases.append(['n1', 'n1', 'n2'])
+        test_cases.append(['n1', 'n2', 'n1'])
+        test_cases.append(['n1', 'n1', 'n5'])
+        test_cases.append(['n1', 'n2', 'n5'])
+
+        test_cases.append(['n2', 'n10', 'n6'])
+        test_cases.append(['n1', 'n10', 'n5'])
+
+        test_cases.append(['n1', 'n1', 'n1'])
+        test_cases.append(['n2', 'n2', 'n2'])
+        test_cases.append(['n3', 'n3', 'n3'])
+        test_cases.append(['n4', 'n4', 'n4'])
+        test_cases.append(['n5', 'n5', 'n5'])
+        test_cases.append(['n6', 'n6', 'n6'])
+        test_cases.append(['n7', 'n7', 'n7'])
+        test_cases.append(['n8', 'n8', 'n8'])
+        test_cases.append(['n9', 'n9', 'n9'])
+        test_cases.append(['n10', 'n10', 'n10'])
+
+        self.dag1.reset_lookup_count()
+        for test_case in test_cases:
+
+            #print "testing (expected, ref1, ref2): " + str(test_case)
+
+            # unpack test case 3-tuple
+            eca_expected, ref1, ref2 = test_case
+
+            eca_slow = gu.dag_find_eca_slow(dag=self.dag1, node1=ref1, node2=ref2)
+
+            self.assertEquals(eca_expected, eca_slow)
+
+        # now get the counter
+        slow_cost = self.dag1.get_lookup_count()
+        print "the slow algo made: " + str(slow_cost) + " many look ups"
+
+        self.dag1.reset_lookup_count()
+        for test_case in test_cases:
+
+            #print "testing (expected, ref1, ref2): " + str(test_case)
+
+            # unpack test case 3-tuple
+            eca_expected, ref1, ref2 = test_case
+
+            eca_found = gu.dag_find_eca_fast(dag=self.dag1, node1=ref1, node2=ref2)
+
+            self.assertEquals(eca_expected, eca_found)
+
+        # now get the counter
+        fast_cost = self.dag1.get_lookup_count()
+        print "the fast algo made: " + str(fast_cost) + " many look ups"
 
 
     def test_inv_dag_is_descendant_of_search(self):
 
         # every node is descendant of root
-        self.assertTrue(gu.inv_dag_is_descendant_of(graph_adjacencies=self.invdag1, parent='n1', node_to_test='n2'))
-        self.assertTrue(gu.inv_dag_is_descendant_of(graph_adjacencies=self.invdag1, parent='n1', node_to_test='n3'))
-        self.assertTrue(gu.inv_dag_is_descendant_of(graph_adjacencies=self.invdag1, parent='n1', node_to_test='n22'))
-        self.assertTrue(gu.inv_dag_is_descendant_of(graph_adjacencies=self.invdag1, parent='n1', node_to_test='n7'))
-        self.assertTrue(gu.inv_dag_is_descendant_of(graph_adjacencies=self.invdag1, parent='n1', node_to_test='n4'))
-        self.assertTrue(gu.inv_dag_is_descendant_of(graph_adjacencies=self.invdag1, parent='n1', node_to_test='n10'))
-        self.assertTrue(gu.inv_dag_is_descendant_of(graph_adjacencies=self.invdag1, parent='n1', node_to_test='n71'))
-        self.assertTrue(gu.inv_dag_is_descendant_of(graph_adjacencies=self.invdag1, parent='n1', node_to_test='n92'))
-        self.assertTrue(gu.inv_dag_is_descendant_of(graph_adjacencies=self.invdag1, parent='n1', node_to_test='n81'))
-        self.assertTrue(gu.inv_dag_is_descendant_of(graph_adjacencies=self.invdag1, parent='n1', node_to_test='n82'))
-        self.assertTrue(gu.inv_dag_is_descendant_of(graph_adjacencies=self.invdag1, parent='n1', node_to_test='n85'))
-        self.assertTrue(gu.inv_dag_is_descendant_of(graph_adjacencies=self.invdag1, parent='n1', node_to_test='n87'))
+        self.assertTrue(gu.inv_dag_is_descendant_of(dag=self.invdag1, parent='n1', node_to_test='n2'))
+        self.assertTrue(gu.inv_dag_is_descendant_of(dag=self.invdag1, parent='n1', node_to_test='n3'))
+        self.assertTrue(gu.inv_dag_is_descendant_of(dag=self.invdag1, parent='n1', node_to_test='n22'))
+        self.assertTrue(gu.inv_dag_is_descendant_of(dag=self.invdag1, parent='n1', node_to_test='n7'))
+        self.assertTrue(gu.inv_dag_is_descendant_of(dag=self.invdag1, parent='n1', node_to_test='n4'))
+        self.assertTrue(gu.inv_dag_is_descendant_of(dag=self.invdag1, parent='n1', node_to_test='n10'))
+        self.assertTrue(gu.inv_dag_is_descendant_of(dag=self.invdag1, parent='n1', node_to_test='n71'))
+        self.assertTrue(gu.inv_dag_is_descendant_of(dag=self.invdag1, parent='n1', node_to_test='n92'))
+        self.assertTrue(gu.inv_dag_is_descendant_of(dag=self.invdag1, parent='n1', node_to_test='n81'))
+        self.assertTrue(gu.inv_dag_is_descendant_of(dag=self.invdag1, parent='n1', node_to_test='n82'))
+        self.assertTrue(gu.inv_dag_is_descendant_of(dag=self.invdag1, parent='n1', node_to_test='n85'))
+        self.assertTrue(gu.inv_dag_is_descendant_of(dag=self.invdag1, parent='n1', node_to_test='n87'))
 
         # root is descendant of no1
-        self.assertFalse(gu.inv_dag_is_descendant_of(graph_adjacencies=self.invdag1, node_to_test='n1', parent='n2'))
-        self.assertFalse(gu.inv_dag_is_descendant_of(graph_adjacencies=self.invdag1, node_to_test='n1', parent='n3'))
-        self.assertFalse(gu.inv_dag_is_descendant_of(graph_adjacencies=self.invdag1, node_to_test='n1', parent='n22'))
-        self.assertFalse(gu.inv_dag_is_descendant_of(graph_adjacencies=self.invdag1, node_to_test='n1', parent='n7'))
-        self.assertFalse(gu.inv_dag_is_descendant_of(graph_adjacencies=self.invdag1, node_to_test='n1', parent='n4'))
-        self.assertFalse(gu.inv_dag_is_descendant_of(graph_adjacencies=self.invdag1, node_to_test='n1', parent='n10'))
-        self.assertFalse(gu.inv_dag_is_descendant_of(graph_adjacencies=self.invdag1, node_to_test='n1', parent='n71'))
-        self.assertFalse(gu.inv_dag_is_descendant_of(graph_adjacencies=self.invdag1, node_to_test='n1', parent='n92'))
-        self.assertFalse(gu.inv_dag_is_descendant_of(graph_adjacencies=self.invdag1, node_to_test='n1', parent='n81'))
-        self.assertFalse(gu.inv_dag_is_descendant_of(graph_adjacencies=self.invdag1, node_to_test='n1', parent='n82'))
-        self.assertFalse(gu.inv_dag_is_descendant_of(graph_adjacencies=self.invdag1, node_to_test='n1', parent='n85'))
-        self.assertFalse(gu.inv_dag_is_descendant_of(graph_adjacencies=self.invdag1, node_to_test='n1', parent='n87'))
+        self.assertFalse(gu.inv_dag_is_descendant_of(dag=self.invdag1, node_to_test='n1', parent='n2'))
+        self.assertFalse(gu.inv_dag_is_descendant_of(dag=self.invdag1, node_to_test='n1', parent='n3'))
+        self.assertFalse(gu.inv_dag_is_descendant_of(dag=self.invdag1, node_to_test='n1', parent='n22'))
+        self.assertFalse(gu.inv_dag_is_descendant_of(dag=self.invdag1, node_to_test='n1', parent='n7'))
+        self.assertFalse(gu.inv_dag_is_descendant_of(dag=self.invdag1, node_to_test='n1', parent='n4'))
+        self.assertFalse(gu.inv_dag_is_descendant_of(dag=self.invdag1, node_to_test='n1', parent='n10'))
+        self.assertFalse(gu.inv_dag_is_descendant_of(dag=self.invdag1, node_to_test='n1', parent='n71'))
+        self.assertFalse(gu.inv_dag_is_descendant_of(dag=self.invdag1, node_to_test='n1', parent='n92'))
+        self.assertFalse(gu.inv_dag_is_descendant_of(dag=self.invdag1, node_to_test='n1', parent='n81'))
+        self.assertFalse(gu.inv_dag_is_descendant_of(dag=self.invdag1, node_to_test='n1', parent='n82'))
+        self.assertFalse(gu.inv_dag_is_descendant_of(dag=self.invdag1, node_to_test='n1', parent='n85'))
+        self.assertFalse(gu.inv_dag_is_descendant_of(dag=self.invdag1, node_to_test='n1', parent='n87'))
 
 
         # a couple others
-        self.assertTrue(gu.inv_dag_is_descendant_of(graph_adjacencies=self.invdag1, parent='n4', node_to_test='n71'))
-        self.assertTrue(gu.inv_dag_is_descendant_of(graph_adjacencies=self.invdag1, parent='n4', node_to_test='n10'))
-        self.assertTrue(gu.inv_dag_is_descendant_of(graph_adjacencies=self.invdag1, parent='n4', node_to_test='n85'))
+        self.assertTrue(gu.inv_dag_is_descendant_of(dag=self.invdag1, parent='n4', node_to_test='n71'))
+        self.assertTrue(gu.inv_dag_is_descendant_of(dag=self.invdag1, parent='n4', node_to_test='n10'))
+        self.assertTrue(gu.inv_dag_is_descendant_of(dag=self.invdag1, parent='n4', node_to_test='n85'))
 
-        self.assertFalse(gu.inv_dag_is_descendant_of(graph_adjacencies=self.invdag1, node_to_test='n3', parent='n10'))
-        self.assertFalse(gu.inv_dag_is_descendant_of(graph_adjacencies=self.invdag1, node_to_test='n22', parent='n4'))
+        self.assertFalse(gu.inv_dag_is_descendant_of(dag=self.invdag1, node_to_test='n3', parent='n10'))
+        self.assertFalse(gu.inv_dag_is_descendant_of(dag=self.invdag1, node_to_test='n22', parent='n4'))
 
 
 if __name__ == '__main__':
